331cbd4f67e712c8836229ebf8bd42d9a44bb50c
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 16:04:24 +0900

    implement countChange

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -45,5 +45,26 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = { 0 }
+  def remove(elem: Int, list: List[Int]): List[Int] = {
+    if (list.isEmpty) {
+      List()
+    } else if (list.head == elem) {
+      list.tail
+    } else {
+      list.head::(remove(elem, list.tail))
+    }
+  }
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    coins.foldLeft(0) ((acc, curr) => {
+      (money, curr) match {
+        case (money, _) if money < 0 => 0
+        case (0, _) => 1
+        case (money, curr) => {
+          val next_coins = remove(curr, coins)
+          acc + countChange(money - curr, next_coins)
+        }
+      }
+    })
+  }
 }
