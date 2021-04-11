01a9e9d4ff56aec60034b0819eb7e2a3128fdded
BeLeap <changseo_jang@korea.ac.kr>
2021-04-11 09:43:56 +0900

    add funsets

--- temp.scala
+++ temp.scala
@@ -1,24 +1,22 @@
-  def innerBalance(chars: List[Char], openedCount: Int): Boolean = {
-    print(chars)
-    print("\n")
-    print(openedCount)
-    print("\n")
-    chars match {
-      case ('('::cs) => innerBalance(cs, openedCount + 1)
-      case (')'::cs) => {
-        if (openedCount - 1 >= 0) {
-          innerBalance(cs, openedCount - 1)
-        } else {
-          false
-        }
-      }
-      case (_::cs) => innerBalance(cs, openedCount)
-      case List() => {
-        openedCount match {
-          case 0 => true
-          case _ => false
-        }
+def remove(elem: Int, list: List[Int]): List[Int] = {
+  if (list.isEmpty) {
+    List()
+  } else if (list.head == elem) {
+    list.tail
+  } else {
+    list.head::(remove(elem, list.tail))
+  }
+}
+
+def countChange(money: Int, coins: List[Int]): Int = {
+  coins.fold(0) ((acc, curr) => {
+    (money, curr) match {
+      case (money, _) if money < 0 => 0
+      case (0, _) => 1
+      case (money, curr) => {
+        val next_coins = remove(curr, coins)
+        acc + countChange(money - curr, next_coins)
       }
     }
-  }
-  def balance(chars: List[Char]): Boolean = innerBalance(chars, 0)
+  })
+}
